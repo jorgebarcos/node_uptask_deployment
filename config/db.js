7bfcaa2a8230674ed2d579934845e00e2351865c
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-18 16:54:15 -0300

    Instalando y Configurando Sequelize

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('uptasknode', 'root', 'root', {
+	host: '127.0.0.1',
+	dialect: 'mysql',
+	port: '3306',
+	operatorAliases: false,
+	define: {
+		timestamps: false
+	},
+
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000
+	}
+});
