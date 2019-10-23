fc450b9a804df92eeb39cf543613603ba12b23a8
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-23 19:59:59 -0300

    Generando una URL única para proyectos

--- models/Proyectos.js
+++ models/Proyectos.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 
 const db = require('../config/db');
 const slug = require('slug');
+const shortid = require('shortid');
 
 const Proyectos = db.define(
 	'proyectos',
@@ -21,7 +22,7 @@ const Proyectos = db.define(
 			beforeCreate(proyecto) {
 				const url = slug(proyecto.nombre).toLowerCase();
 
-				proyecto.url = url;
+				proyecto.url = `${url}-${shortid.generate()}`;
 			}
 		}
 	}
