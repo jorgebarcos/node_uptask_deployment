41ebb1dc78b40bc5db6869299ea82633e2569d0b
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-23 19:17:05 -0300

    Permitiendo que Sequelize Genere las Tablas Automaticamente

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('uptasknode', 'root', 'root', {
+const db = new Sequelize('uptasknode', 'root', 'root', {
 	host: '127.0.0.1',
 	dialect: 'mysql',
 	port: '3306',
@@ -16,3 +16,5 @@ const sequelize = new Sequelize('uptasknode', 'root', 'root', {
 		idle: 10000
 	}
 });
+
+module.exports = db;
