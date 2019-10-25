03c360a080821f1a7c58522e5072391bcbebd8d3
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-25 19:43:06 -0300

    Guardando los cambios cuando un proyecto es modificado en su nombre

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -42,7 +42,7 @@ exports.nuevoProyecto = async (req, res) => {
 		// No hay errores
 		// Insertar en la BD.
 
-		const proyecto = await Proyectos.create({ nombre });
+		await Proyectos.create({ nombre });
 		res.redirect('/');
 	}
 };
@@ -85,4 +85,34 @@ exports.formularioEditar = async (req, res) => {
 		proyectos,
 		proyecto
 	});
+};
+
+exports.actualizarProyecto = async (req, res) => {
+	const proyectos = await Proyectos.findAll();
+	// Enviar a la consola lo que el usuario escriba
+	//console.log(req.body);
+
+	// validar que tengamos algo en el input
+	const { nombre } = req.body;
+
+	let errores = [];
+
+	if (!nombre) {
+		errores.push({ texto: 'Agrega un Nombre al Proyecto' });
+	}
+
+	// si hay errores
+	if (errores.length > 0) {
+		res.render('nuevoProyecto', {
+			nombrePagina: 'Nuevo Proyecto',
+			errores,
+			proyectos
+		});
+	} else {
+		// No hay errores
+		// Insertar en la BD.
+
+		await Proyectos.update({ nombre: nombre }, { where: { id: req.params.id } });
+		res.redirect('/');
+	}
 };
