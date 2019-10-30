6ce23af11382380a1b22a73a1285dd14cec28a83
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-30 16:44:12 -0300

    Almacenando las Tareas en la base de datos

--- controllers/tareasController.js
+++ controllers/tareasController.js
@@ -1,3 +1,28 @@
-exports.agregarTarea = (req, res) => {
-	res.send('Enviado');
+const proyectos = require('../models/Proyectos');
+const Tareas = require('../models/Tareas');
+
+exports.agregarTarea = async (req, res, next) => {
+	// obtenemos el proyecto actual
+	const proyecto = await proyectos.findOne({
+		where: {
+			url: req.params.url
+		}
+	});
+	// Leer el valor del input
+	const { tarea } = req.body;
+
+	// Estado 0 = incompleto y ID del Proyecto
+
+	const estado = 0;
+	const proyectoId = proyecto.id;
+
+	// Insertar en la base de datos
+	const resultado = await Tareas.create({ tarea, estado, proyectoId });
+
+	if (!resultado) {
+		return next();
+	}
+
+	// redireccionar
+	res.redirect(`/proyectos/${req.params.url}`);
 };
