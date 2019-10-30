5f6d01eb7f5a911d255df28ea2a8af88ffa0cc34
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-30 19:39:30 -0300

    Creando el Modelo de Usuarios

---
+++ models/Usuarios.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+const Proyectos = require('./Proyectos');
+
+const Usuario = db.define('usuario', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	email: {
+		type: Sequelize.STRING(60),
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING(60),
+		allowNull: false
+	}
+});
+
+Usuarios.hasMany(Proyectos);
+
+module.exports = Usuarios;
