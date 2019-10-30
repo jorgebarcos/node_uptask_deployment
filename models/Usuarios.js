8444fd99f43f840ece34b5ef82d65124fd7a4986
Jorge Barcos <jorgebarcos88@gmail.com>
2019-10-30 20:06:27 -0300

    Hashear los Passwords con Node

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,22 +1,33 @@
 const Sequelize = require('sequelize');
 const db = require('../config/db');
 const Proyectos = require('../models/Proyectos');
+const bcrypt = require('bcrypt-nodejs');
 
-const Usuarios = db.define('usuario', {
-	id: {
-		type: Sequelize.INTEGER,
-		primaryKey: true,
-		autoIncrement: true
+const Usuarios = db.define(
+	'usuario',
+	{
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		email: {
+			type: Sequelize.STRING(60),
+			allowNull: false
+		},
+		password: {
+			type: Sequelize.STRING(60),
+			allowNull: false
+		}
 	},
-	email: {
-		type: Sequelize.STRING(60),
-		allowNull: false
-	},
-	password: {
-		type: Sequelize.STRING(60),
-		allowNull: false
+	{
+		hooks: {
+			beforeCreate(usuario) {
+				usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10));
+			}
+		}
 	}
-});
+);
 
 Usuarios.hasMany(Proyectos);
 
