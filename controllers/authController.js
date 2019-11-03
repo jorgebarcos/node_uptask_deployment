9a541af380e0f531225c9e89c820fa6d55a7b730
Jorge Barcos <jorgebarcos88@gmail.com>
2019-11-03 09:38:39 -0300

    Hasheando, Almacenando el Nuevo Password y limpiando el Token anterior

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const Usuarios = require('../models/Usuarios');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 const crypto = require('crypto');
+const bcrypt = require('bcrypt-nodejs');
 
 exports.autenticarUsuario = passport.authenticate('local', {
 	successRedirect: '/',
@@ -95,4 +96,15 @@ exports.actualizarPassword = async (req, res) => {
 		req.flash('error', 'No Válido');
 		res.redirect('/reestablecer');
 	}
+
+	// hashear el nuevo password
+	usuario.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+	usuario.token = null;
+	usuario.expiracion = null;
+
+	// Guardamos el nuevo passsword
+	await usuario.save();
+
+	req.flash('correcto', 'Tu password se ha modificado correctamente');
+	res.redirect('/iniciar-sesion');
 };
