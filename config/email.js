9a13cd0f19adc12bd109e0e2ba9cf59fbf3ddecc
Jorge Barcos <jorgebarcos88@gmail.com>
2019-11-03 10:08:27 -0300

    Configurando NodeMailer y enviando un mail de prueba

---
+++ config/email.js
@@ -0,0 +1,6 @@
+module.exports = {
+	user: 'e34f6cafc894de',
+	pass: 'e3278fe4cc8ae7',
+	host: 'smtp.mailtrap.io',
+	port: '2525'
+};
---
+++ handlers/email.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const htmlToText = require('html-to-text');
+const util = require('util');
+const emailConfig = require('../config/email');
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+	// Generate test SMTP service account from ethereal.email
+	// Only needed if you don't have a real mail account for testing
+	let testAccount = await nodemailer.createTestAccount();
+
+	let transporter = nodemailer.createTransport({
+		host: emailConfig.host,
+		port: emailConfig.port,
+		secure: false, // true for 465, false for other ports
+		auth: {
+			user: emailConfig.user, // generated ethereal user
+			pass: emailConfig.pass // generated ethereal password
+		}
+	});
+
+	// send mail with defined transport object
+	let info = await transporter.sendMail({
+		from: '"UpTask" <no-reply@uptask.com>', // sender address
+		to: 'correo@correo.com', // list of receivers
+		subject: 'Password Reset', // Subject line
+		text: 'Hola', // plain text body
+		html: '<b>Hola</b>' // html body
+	});
+
+	console.log('Message sent: %s', info.messageId);
+	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+	// Preview only available when sending through an Ethereal account
+	console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+	// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
